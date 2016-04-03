8126aca9cc70af9ed0dc49530aa06cff8020fbea
Rifat Arefin <rifatarefin19@gmail.com>
2016-04-03 08:39:17 +0600

    main task done6

--- FINAL.asm
+++ FINAL.asm
@@ -9,7 +9,9 @@
     
 
 RESULT  DB  'Case #'
-CASE    DB  ?,'$'
+CASE    DB  '1',': $'
+T       DB  ?
+TC      DB  1
 STR1    DW  0              ;main binary
 TMP     DW  0              ;remainder
 CHAR    DW  0              ;no of bits
@@ -23,7 +25,30 @@ MAIN PROC
    
    
     mov ax,@data
-    mov ds,ax   
+    mov ds,ax
+    
+    MOV AH,1
+    INT 21H
+    MOV T,AL
+    SUB T,'0'
+START:
+    
+    MOV AL,T
+    CMP TC,AL
+    JG  END1
+    
+    MOV DL,0DH
+    MOV AH,2
+    INT 21H
+    MOV DL,0AH
+    MOV AH,2
+    INT 21H    
+    
+    MOV STR1,0
+    MOV CHAR,0
+    MOV N,0
+    MOV SIGN,0
+     
     MOV AH,1
     INT 21H
     CMP AL,'-'
@@ -106,6 +131,7 @@ STRINGP:
     MOV AH,9
     LEA DX,RESULT
     INT 21H
+    
     CMP CX,0
     JE ZERO
        
@@ -139,6 +165,12 @@ ZERO:
     INT 21H
     
 end:
+
+    INC TC
+    INC CASE
+    JMP START 
+
+END1:
 
     MOV AH,4CH
     INT 21H
