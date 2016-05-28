2b6a93a5125bfb4eee0f83a066baea7ab151fcc5
Rifat Arefin <rifatarefin19@gmail.com>
2016-05-29 00:06:24 +0600

    WINNER

--- TICTAC .asm
+++ TICTAC .asm
@@ -4,16 +4,19 @@
 A DW ?
 B Dw 80        ;b,c hor highlight
 c dw 80
-P DB 13
+P DB 13        ;P,Q FOR TEXT COORDINATE
 Q DB 7
 X DW 80
 Y DW 80
 GRID DB 9 DUP(0)
 ID DW 0
-draw_row Macro 
+PLAYER1 DB 'PLAYER 1 WINS$'
+PLAYER2 DB 'PLAYER 2 WINS$'
+
+draw_row Macro            ;DRAWS BOX ON (B,C)
     Local l1,l2,L3,L4
     ; draws a line in row y from col x to col 300
-    
+    MOV AH, 0CH
     MOV CX, x
     MOV DX, y
     MOV A,CX
@@ -56,10 +59,279 @@ L4:
     
     EndM
     
-DRAW_BOX MACRO
+RESULT MACRO
+    LOCAL D1,D2,D3,D4,D5,D6,D7,E0,E1,E2,E3,E4,E5,E6,E7
+    MOV BX,0
+    CMP GRID[BX],1
+    JNE D1
+    INC BX
+    CMP GRID[BX],1
+    JNE D1
+    INC BX
+    CMP GRID[BX],1
+    JNE D1
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H
+    JMP OVER
+    
+D1:
+    MOV BX,3
+    CMP GRID[BX],1
+    JNE D2
+    INC BX
+    CMP GRID[BX],1
+    JNE D2
+    INC BX
+    CMP GRID[BX],1
+    JNE D2
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+    
+    
+d2:
+    MOV BX,6
+    CMP GRID[BX],1
+    JNE D3
+    INC BX
+    CMP GRID[BX],1
+    JNE D3
+    INC BX
+    CMP GRID[BX],1
+    JNE D3
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+    
+D3:
+    MOV BX,0
+    CMP GRID[BX],1
+    JNE D4
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D4
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D4
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+D4:
+    MOV BX,1
+    CMP GRID[BX],1
+    JNE D5
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D5
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D5
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+D5:
+    MOV BX,2
+    CMP GRID[BX],1
+    JNE D6
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D6
+    ADD BX,3
+    CMP GRID[BX],1
+    JNE D6
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+D6:
+    MOV BX,2
+    CMP GRID[BX],1
+    JNE D7
+    ADD BX,2
+    CMP GRID[BX],1
+    JNE D7
+    ADD BX,2
+    CMP GRID[BX],1
+    JNE D7
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+D7:
+    MOV BX,0
+    CMP GRID[BX],1
+    JNE E0
+    ADD BX,4
+    CMP GRID[BX],1
+    JNE E0
+    ADD BX,4
+    CMP GRID[BX],1
+    JNE E0
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER2
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E0:
 
+    MOV BX,0
+    CMP GRID[BX],2
+    JNE E1
+    INC BX
+    CMP GRID[BX],2
+    JNE E1
+    INC BX
+    CMP GRID[BX],2
+    JNE E1
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H
+    JMP OVER
+    
+E1:
+    MOV BX,3
+    CMP GRID[BX],2
+    JNE E2
+    INC BX
+    CMP GRID[BX],2
+    JNE E2
+    INC BX
+    CMP GRID[BX],2
+    JNE E2
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+    
+    
+E2:
+    MOV BX,6
+    CMP GRID[BX],2
+    JNE E3
+    INC BX
+    CMP GRID[BX],2
+    JNE E3
+    INC BX
+    CMP GRID[BX],2
+    JNE E3
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+    
+E3:
+    MOV BX,0
+    CMP GRID[BX],2
+    JNE E4
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E4
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E4
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E4:
+    MOV BX,1
+    CMP GRID[BX],2
+    JNE E5
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E5
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E5
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E5:
+    MOV BX,2
+    CMP GRID[BX],2
+    JNE E6
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E6
+    ADD BX,3
+    CMP GRID[BX],2
+    JNE E6
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E6:
+    MOV BX,2
+    CMP GRID[BX],2
+    JNE E7
+    ADD BX,2
+    CMP GRID[BX],2
+    JNE E7
+    ADD BX,2
+    CMP GRID[BX],2
+    JNE E7
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E7:
+    MOV BX,0
+    CMP GRID[BX],2
+    JNE E8
+    ADD BX,4
+    CMP GRID[BX],2
+    JNE E8
+    ADD BX,4
+    CMP GRID[BX],2
+    JNE E8
+    MOV AX, 3
+    INT 10h
+    LEA DX,PLAYER1
+    MOV AH,9
+    INT 21H 
+    JMP OVER
+E8:
+
+    ENDM
 
     
+DRAW_BOX MACRO
+
     
 ; draw line pixel by pixel 
      
@@ -108,6 +380,7 @@ CHAR_SHOW MACRO
         INT 10h
         
    ENDM
+   
 .Code
 main Proc 
 
@@ -117,26 +390,24 @@ main Proc
 ; set CGA 320x200 high res mode
     MOV AX,13
     INT 10h
-    
-    
-    
-    
+
+        
     
 kkk:                          ;GAME LOOP
     ;B=ROW,C=COLUMN
     ;DRAW HIGHLIGHTED BOX
-    
+    RESULT
     DRAW_BOX
     
-    
-    
-    MOV AL,15              
     MOV BX,B
     MOV X,BX
-    ;INC X
+    
     MOV BX,C
     MOV Y,BX
-    ;INC Y
+    
+    MOV AL,15       ;WHITE       
+    
+    
     DRAW_ROW
     
         
@@ -145,14 +416,12 @@ kkk:                          ;GAME LOOP
     INT 16h
     CMP AH,77
     JE RIGHT
-    CMP AH,75
-    JE LEFT
-    CMP AH,72
-    JE UP
+    
     JMP GOL
     
  
 RIGHT:
+    
     cmp B,180
     JGE TT
     ADD P,6
@@ -165,18 +434,7 @@ TT:
     SUB ID,2
     JMP KKK
     
-LEFT:
-    cmp B,80
-    JLE TT2
-    SUB B,50
-    SUB P,6
-    DEC ID
-    JMP KKK
-TT2:
-    MOV B,180
-    MOV P,25
-    ADD ID,2
-    JMP KKK
+
     
 UP:
     cmp C,80
@@ -205,13 +463,16 @@ DDD2:
     JMP KKK 
 
 GOL:
+    
+    CMP AH,72
+    JE UP
     CMP AH,80
     JE DOWN
     CMP AH,18H
     JE OOP
     CMP AH,2DH
     JE AXE
-    JMP KKK
+    JMP PKP
     
 
 OOP:
@@ -242,11 +503,28 @@ AXE2:
     MOV BL,1
     CHAR_SHOW
     JMP KKK   
+    
+PKP:
+    CMP AH,75
+    JE LEFT
+    JMP KKK
+    
+LEFT:
+    cmp B,80
+    JLE TT2
+    SUB B,50
+    SUB P,6
+    DEC ID
+    JMP KKK
+TT2:
+    MOV B,180
+    MOV P,25
+    ADD ID,2
+    JMP KKK 
 
     
-        MOV AX, 3
-        INT 10h
-        
+OVER:   
+     
         MOV AH, 4CH
         INT 21h
 main EndP
